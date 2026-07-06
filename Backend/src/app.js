be9844ec60c013b997a8bd40653ee1840a79095c
Ajay Gour <theajaygour@gmail.com>
2026-07-06 23:17:49 +0530

    ok

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"https://i-moddify.vercel.app/ || http://localhost:5173",
+    origin:"https://i-moddify.vercel.app/ ",
     credentials:true
 }))
 
